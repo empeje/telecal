ab6ca55ea72d826c0122f106bfeb065a3dd98713
empeje <11813607+empeje@users.noreply.github.co>
2023-11-08 04:00:26 +0100

    Add WIP page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,29 +14,22 @@ const Home: NextPage = () => {
   const initData = useTelegramInitData();
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-telegram-white">
-      <div>{JSON.stringify(initData)}</div>
       <Head>
-        <title>Next.js + Tailwind CSS + Telegram&apos;s Web App</title>
+        <title>Telecal</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+
       <main className="flex w-full flex-1 flex-col items-center justify-center px-4 text-center">
         <span className="text-2xl font-bold text-telegram-black">
-          This is a starter template using Next.js and Tailwind CSS for Telegram&apos;s Web Apps.
+          Welcome {initData.user?.username}
         </span>
-        <DynamicMainButton text="Submit" onClick={() => alert('submitted')} />
+        <p className="text-telegram-black">
+          This menu is still not available. Please contact the <a className="text-blue-500" href="https://t.me/empeje">author</a> 😿!</p>
       </main>
 
-      <footer className="flex h-20 w-full items-center justify-center border-t border-t-telegram-black">
-        <a
-          className="flex items-center justify-center gap-2 text-telegram-black"
-          href="https://github.com/mauriciobraz/next.js-telegram-webapp"
-          target="_blank"
-          rel="noopener noreferrer">
-          Powered by{' '}
-          <span className="text-telegram-link">mauriciobraz/next.js-telegram-webapp</span>
-          <GithubSvg className="h-6 w-6 fill-telegram-link" />
-        </a>
+      <footer className="flex h-20 w-full items-center justify-center border-t border-t-telegram-black text-telegram-black">
+        <p>Made with love by empeje</p>
       </footer>
     </div>
   );
