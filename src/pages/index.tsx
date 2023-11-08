89f56dcc4d6e5a511fd31cce95985d3b93ddb61b
empeje <11813607+empeje@users.noreply.github.co>
2023-11-08 04:04:05 +0100

    Fix compiles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const DynamicMainButton = dynamic(
 );
 
 const Home: NextPage = () => {
-  const initData = useTelegramInitData();
+  const initData: any = useTelegramInitData();
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-telegram-white">
       <Head>
@@ -19,13 +19,17 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
       <main className="flex w-full flex-1 flex-col items-center justify-center px-4 text-center">
         <span className="text-2xl font-bold text-telegram-black">
           Welcome {initData.user?.username}
         </span>
         <p className="text-telegram-black">
-          This menu is still not available. Please contact the <a className="text-blue-500" href="https://t.me/empeje">author</a> 😿!</p>
+          This menu is still not available. Please contact the{' '}
+          <a className="text-blue-500" href="https://t.me/empeje">
+            author
+          </a>{' '}
+          😿!
+        </p>
       </main>
 
       <footer className="flex h-20 w-full items-center justify-center border-t border-t-telegram-black text-telegram-black">
